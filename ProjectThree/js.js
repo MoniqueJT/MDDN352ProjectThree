9679b1aa3bc09e2bbdca6a82cf38a6233975b10f
MoniqueJT <trassmonique@gmail.com>
2017-06-07 01:21:51 +1200

    Add files via upload

--- ProjectThree/js.js
+++ ProjectThree/js.js
@@ -2,13 +2,16 @@ $(document).ready(function(){
 
 //API 
 var _FreeApiBaseURL = 'http://api.worldweatheronline.com/free/v1/';
-var _FreeApiKey = 'xkq544hkar4m69qujdgujn7w';
+var _FreeApiKey = '310facabc35e4f16a4913539172804';
 
 function JSONP_MarineWeather(input) {
     var url = _FreeApiBaseURL + "marine.ashx?q=" + input.query + "&format=" + input.format + "&fx=" + input.fx + "&key=" + _FreeApiKey;
 
     jsonP(url, input.callback);
+
+
 }
+
 
 // -------------------------------------------
 
@@ -18,11 +21,13 @@ function jsonP(url, callback) {
         type: 'GET',
         url: url,
         async: false,
+        cache: true,
         contentType: "application/json",
         jsonpCallback: callback,
         dataType: 'jsonp',
         success: function (json) {
             console.dir('success');
+            console.log(JSON.stringify(json));
         },
         error: function (e) {
             console.log(e.message);
@@ -37,7 +42,67 @@ function jsonP(url, callback) {
 
 
 
-
+  $add = $('#add-user');
+  
+  setTimeout(function() {
+    $add.addClass('hidden');
+  }, 200)
+  var numRows = 2;
+  
+  $('input').keyup(function() {
+    var isReady = false;
+    
+    $('input').each(function() {
+      if ($(this).val() != '') {
+        isReady = true;  
+      }
+    });
+  
+    if (isReady) {
+      $add.removeClass('hidden'); 
+    }
+    else {
+      $add.addClass('hidden');
+    }
+  });
+  
+  $add.on('click', function() {
+    var $scope = $('.tb-entry');
+    var name = $scope.find('#name').val();
+    var age = $scope.find('#age').val();
+    var favNum = $scope.find('#fav-num').val();
+    
+    $scope.find('input').attr('disabled', 'true');
+    $scope.find('input').val('');
+    
+    $scope.after(
+      '<div class="tb-data row row-'+numRows+'">' +
+      '<ul class="data-options">' +
+      '<li><a href="#"><i class="fa fa-pencil"></i> Edit</a></li>' +
+      '<li><a href="#"><i class="fa fa-trash-o"></i> Delete</a></li>' +
+      '</ul>' +
+      '<div class="col">' +
+      name +
+      '</div>' +
+      '<div class="col">' +
+      age +
+      '</div>' +
+      '<div class="col last">' +
+      favNum +
+      '</div>' +
+      '</div>');
+    
+    $('.row-'+numRows).hide();  
+    $('.row-'+numRows).slideDown(300);
+    
+    setTimeout(function() {
+      $scope.find('input').removeAttr('disabled');
+      $scope.find('input').first().focus();
+      $add.addClass('hidden');
+    }, 300);
+    
+    numRows++;
+  });
 
 
 
